875167ea16606721bd16c8f7d9a6e405f79a655c
hydraadmin <hydraadmin@hydra.bodleian.ox.ac.uk>
2013-03-27 16:18:19 +0000

    Created a book, page and journal article model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OraIngest::Application.config.secret_token = '3f287cdc7512e75585502294cb025420468aeab4ea993f6521e9118e91768f5710814723cd01be4c6f66cd910c5df4c9a34cca51b1f1dd65a318ef0e85165276'
---
+++ features/home_page.feature
@@ -0,0 +1,7 @@
+Feature: Homepage
+  I want the home page to reflect localizations properly
+
+  Scenario: home page text
+    When I am on the home page
+    Then I should not see "override"
+    And I should see "ORA Contribute"
