60260bc057931e8685d28591cadde748cd3c914a
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2013-07-02 16:19:12 +0100

    Initial sufia commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OraHydra::Application.config.secret_token = 'ae67093fed1eadb55acebf660d1e0803bdae5e1d68f6d381dd78940c8722a3e52cb91eba75654a708d1ef59294b8080cbb709924845bbfc46470196064729079'
