51728dc92e265c43685692f9fd65bfc1e6a0f0fa
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2013-07-04 11:25:59 +0100

    Added descMetadata to generic files

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -1,9 +1,14 @@
 require "datastreams/workflow_rdf_datastream"
+require "datastreams/generic_file_rdf_datastream"
+require "person"
+require "oxford_terms"
+require "rdf"
+
 class GenericFile < ActiveFedora::Base
   include Sufia::GenericFile
-  attr_accessible :workflows, :workflows_attributes
-  
+  has_metadata :name => "descMetadata", :type => GenericFileRdfDatastream
   has_metadata :name => "workflowMetadata", :type => WorkflowRdfDatastream
-  
-  delegate_to "workflowMetadata",  [:workflows, :workflows_attributes]
+  has_many :authors, :property=> :has_author, :class_name=>"Person"
+  has_many :contributors, :property=> :has_contributor, :class_name=>"Person"
+  has_many :copyright_holders, :property=> :has_copyright_holder, :class_name=>"Person"
 end
