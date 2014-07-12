1aaa8f77075b052a84115085111ceeddce61c5db
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2014-07-12 13:55:01 +0100

    Added back the custom javascrcipt for cloning, and the changes to the views supporting this change retaining the essential lines in the js from the original. Tested to make sure the values of all of the repeated fields gets submitted and autocomplete works on repeated fields

--- app/assets/javascripts/sufia/multiForm.js
+++ app/assets/javascripts/sufia/multiForm.js
@@ -2,53 +2,73 @@
 
   $.fn.multiForm = function( options ) {  
 
-    // Create some defaults, extending them with any options that were provided
-    var settings = $.extend( { }, options);
+  // Create some defaults, extending them with any options that were provided
+  var settings = $.extend( { }, options);
 
-    function addField() {
-      
-      count = $(this).closest('.control-group').find('.controls').size();
-      var cloneId = this.id.replace("submit", "clone");
-      var newId = this.id.replace("submit", "elements");
-      var cloneElem = $('#'+cloneId).clone();
-      // change the add button to a remove button
-      var plusbttn = cloneElem.find('#'+this.id);
-      plusbttn.html('-<span class="accessible-hidden">remove this '+ this.name.replace("_", " ") +'</span>');
-      plusbttn.on('click',removeField);
-
-
-      // remove the help tag on subsequent added fields
-      cloneElem.find('.formHelp').remove();
-      cloneElem.find('i').remove();
-      cloneElem.find('.modal-div').remove();
-
-      //clear out the value for the element being appended
-      //so the new element has a blank value
-      // Note: there may be more than one input field. Example:
-      //   creator_name
-      //   creator_role
-      inputFields = cloneElem.find('input')
-      $.each(inputFields, function(n, tf) {
-        newName = $(tf).attr('name').replace('[0]', '['+count+']');
-        $(tf).attr('name', newName).attr("value", "").attr("required", false);
-        $(tf).val("");
-      })
-      cloneElem.attr("autocomplete", "");
-
-      if (settings.afterAdd) {
-        settings.afterAdd(this, cloneElem)
-      }
-
-      $('#'+newId).append(cloneElem);
-       
-      if (this.id.toLowerCase().indexOf("subject") >= 0) {
-        $( cloneElem ).find( 'input.subjectLabel' ).autocomplete(autocompleteSubject).data("autocomplete")._renderItem = renderSubject;
-      }
-
-      // Focus on the first inout element
-      cloneElem.find('input[type=text]').first().focus();
-      return false;
+  function addField() {
+    var parentId = this.id.replace("button", "clone");
+    var groupId = this.id.split("_",1)
+    var count = $("#" + groupId + " div").size();
+    var lastId = $("#" + groupId + " div").last().attr("id");
+    lastId = lastId.replace(groupId+"_clone_", "");
+    if ($.isNumeric(lastId)) {
+      count = parseInt(lastId) + 1;
     }
+    var newId = groupId + "_clone_" + count;
+    var cloneElem = $('#'+ parentId).clone();
+    cloneElem.attr('id', newId);
+    
+    // change the add button to a remove button
+    var plusbttn = cloneElem.find('#'+this.id);
+    plusbttn.className = "remover btn";
+    plusbttn.attr('id', groupId + "_button_" + count);
+    plusbttn.attr('onclick', '');
+    plusbttn.html('-<span class="accessible-hidden">remove this '+ this.name.replace("_", " ") +'</span>');
+    plusbttn.on('click',removeField);
+    
+    // remove the help tag on subsequent added fields
+    cloneElem.find('.formHelp').remove();
+    cloneElem.find('i').remove();
+    cloneElem.find('.modal-div').remove();
+    
+    //clear out the value for the element being appended
+    //so the new element has a blank value
+    inputFields = cloneElem.find('input')
+    $.each(inputFields, function(n, tf) {
+      newName = $(tf).attr('id').replace('0', count);
+      $(tf).val("");
+      $(tf).attr('id', newName).attr("required", false);
+    })
+    selectFields = cloneElem.find('select')
+    $.each(selectFields, function(n, tf) {
+      newName = $(tf).attr('id').replace('0', count);
+      $(tf).val("");
+      $(tf).attr('id', newName).attr("required", false);
+    })
+  
+    if (settings.afterAdd) {
+      settings.afterAdd(this, cloneElem)
+    }
+
+    // Add the cloned element to the page
+    $("#"+groupId).append(cloneElem);
+    
+    //add autocomplete option
+    if (groupId == "subject") {
+      inputId = "subjectLabel"+count;
+      console.log(inputId);
+      $( "#"+inputId ).autocomplete(autocompleteSubject).data("autocomplete")._renderItem = renderSubject;
+    } else if (groupId == "language") {
+      inputId = "languageLabel"+count;
+      console.log(inputId);
+      $( "#"+inputId ).autocomplete(autocompleteLanguage).data("autocomplete")._renderItem = renderLanguage;
+    }
+  
+    // Focus on the cloned element 
+    cloneElem.find('input[type=text]').first().focus();
+    return false;
+  }
+
 
     function removeField () {
       // get parent and remove it
