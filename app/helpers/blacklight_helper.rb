e3241ad4eb3594c38d62cd5da940083076373b7c
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2014-10-16 16:58:31 +0100

    Added a helper function to generate the url for each of the models

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -2,7 +2,29 @@ module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
   def application_name
-    "WGBH"
+    "ORA"
+  end
+
+  def document_url(model, action, id="")
+    actions = ['index', 'show', 'new', 'edit', 'destroy']
+    models = { "Article" => 'articles', "DatasetAgreement" => "dataset_agreements", "Dataset" => "datasets" }
+    path = ""
+    puts "============================"
+    puts actions.include?(action)
+    puts models.keys.include?(model)
+    puts "============================"
+    if actions.include?(action) && models.keys.include?(model)
+      begin
+        if id
+          path = url_for(:controller => models[model], :action=>action, :id => id)
+        else
+          path = url_for(:controller => models[model], :action=>action)
+        end
+      rescue ActionController::UrlGenerationError => e
+        path = ""
+      end
+    end 
+    path
   end
 
 end
