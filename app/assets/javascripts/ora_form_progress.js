1059095e7df819fa40581c23f16e2088f4350921
Clinton Montague <clinton@oneltd.co.uk>
2014-11-16 22:01:51 +0000

    Complete form progress tracker. Add new partial for the tracker so it can be reused

--- app/assets/javascripts/ora_form_progress.js
+++ app/assets/javascripts/ora_form_progress.js
@@ -1,37 +1,75 @@
 (function($) {
-  $.expr[':'].oraValid = function(/*elem, index, match*/) {
-    return Math.round(Math.random());
-  };
+  /**
+   * Counts the totla number of complete inputs (complete counts as checked, 
+   * selected, or non-empty
+   */
+  function countComplete($inputs) {
+    var valid_count = 0;
+    $inputs.each(function() {
+      if ($(this).attr("type") == "radio" || $(this).attr("type") == "checkbox") {
+        if (this.checked) {
+          valid_count += 1;
+        }
+      } else if (!!$(this).val()) {
+        valid_count += 1;
+      }
+    });
+    return valid_count;
+  }
 
-  $(document).on("ora.formupdate", function(event, $form) {
-    var inputs = $form.find("[data-progress]"),
+  /**
+   * Counts the total number of unique inputs. Unique will discount things like
+   * radios where there are a few inputs all with the same name, but should be
+   * thought of as one item.
+   */
+  function countUnique($inputs) {
+    return $.unique($.map($inputs, function(input) {
+        return input.name;
+      })).length
+  }
+
+
+  function updateProgress() {
+    var inputs = $("[data-progress]"),
       sections = $.unique($.map(inputs, function(input) {
           return $(input).data("progress");
         })),
       section_counts = {};
-      
-      $.each(sections, function(idx, section) {
-          var section_inputs = inputs.filter('[data-progress="' + section + '"]');
 
-          section_counts[section] = {
-            count: section_inputs.length,
-            percent: 100 * (section_inputs.filter(":oraValid").length) / section_inputs.length
-          };
-        });
+    $.each(sections, function(idx, section) {
+        var section_inputs = inputs.filter('[data-progress="' + section + '"]'),
+          input_count = countUnique(section_inputs);
 
-      $("[data-progress-link]").each(function() {
-        var $this = $(this),
-          section = $(this).data("progress-link");
-
-        if (typeof section_counts[section] !== "undefined") {
-          //$(this).find(".percentage").css("width", section_counts[section].percent + "%");
-          $this.find(".percentage").stop().animate({
-            width: section_counts[section].percent + "%"
-          }, {
-            duration: 500
-          });
-        }
+        section_counts[section] = {
+          count: input_count,
+          percent: 100 * (countComplete(section_inputs)) / input_count
+        };
       });
 
-  });
+    $(".chart").each(function() {
+      $(this).data('easyPieChart').update(100 * countComplete(inputs) / countUnique(inputs));
+    });
+
+
+    $("[data-progress-link]").each(function() {
+      var $this = $(this),
+        section = $(this).data("progress-link");
+
+      if (typeof section_counts[section] !== "undefined") {
+        //$(this).find(".percentage").css("width", section_counts[section].percent + "%");
+        $this.find(".percentage").stop().animate({
+          width: section_counts[section].percent + "%"
+        }, {
+          duration: 500
+        });
+      }
+    });
+  }
+
+
+  /**
+   * Let's do this 
+   */
+  $(document).on("change", "[data-progress]", updateProgress);
+  $(document).ready(updateProgress);
 })(jQuery);
