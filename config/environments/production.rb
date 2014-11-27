5d0c0b8c885c3dae8ca2eb4a567ef1f7f6377ce2
Michael Davis <michael.davis@bodleian.ox.ac.uk>
2014-11-27 14:19:52 +0000

    Rails doesn't like font filenames with hyphens

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,12 +20,6 @@ OraHydra::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Add the fonts path
-  #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  
-  # Precompile additional assets
-  #config.assets.precompile += %w( .svg .eot .woff .ttf )
-
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
@@ -73,3 +67,4 @@ OraHydra::Application.configure do
 
   config.eager_load = true
 end
+
