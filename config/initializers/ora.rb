16e29c08905588c0e157b29c506b3f43f6b68845
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2014-11-28 15:41:53 +0000

    Data dir in dev is different from prod

--- config/initializers/ora.rb
+++ config/initializers/ora.rb
@@ -354,7 +354,7 @@ Sufia.config do |config|
 
   config.contact_email = "ora@bodleian.ox.ac.uk"
   config.from_email = "no-reply@bodleian.ox.ac.uk"
-  config.data_root_dir = "/data/oradeposit/"
+  config.data_root_dir = "/data/"
   config.cud_base_url = "http://10.0.0.203"
 end
 
