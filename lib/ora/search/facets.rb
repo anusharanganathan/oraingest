b42b075fa9e157ae8ceb7ce73a21a6af30a54a8b
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2014-12-04 17:05:35 +0000

    Moved facet for type into the global config for facets and removed from controller

--- lib/ora/search/facets.rb
+++ lib/ora/search/facets.rb
@@ -16,6 +16,7 @@ module Ora
           config.add_facet_field solr_name("desc_metadata__language", :facetable), :label => "Language", :limit => 5
           config.add_facet_field solr_name("desc_metadata__based_near", :facetable), :label => "Location", :limit => 10
           config.add_facet_field solr_name("desc_metadata__publisher", :facetable), :label => "Publisher", :limit => 10
+          config.add_facet_field "active_fedora_model_ssi", :label => "Type", :limit => 10#, :pivot => [Solrizer.solr_name("desc_metadata__type", :symbol)]
           #config.add_facet_field solr_name("file_format", :facetable), :label => "File Format", :limit => 10
           # Have BL send all facet field names to Solr, which has been the default
           # previously. Simply remove these lines if you'd rather use Solr request
