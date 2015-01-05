d9ea0642cb2ec028f41a3129a6da12fc5d77d198
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-01-05 14:50:54 +0000

    Added default config options for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,5 +69,7 @@ OraHydra::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.eager_load = true
+
+  Rails.application.routes.default_url_options[:host] = 'oradeposit.bodleian.ox.ac.uk'
 end
 
