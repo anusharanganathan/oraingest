3239c145c9daf789c0b68d5a6bc83f76c235652a
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-04-09 17:40:03 +0100

    Modified application.js to not inherit from sufia and included jquery and bootstrap js along with custom js Included and required the jquery-validate plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,17 +12,43 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui-1.9.2/jquery.ui.core
+//= require jquery-ui-1.9.2/jquery.ui.widget
+//= require jquery-ui-1.9.2/jquery.ui.menu
+//= require jquery-ui-1.9.2/jquery.ui.autocomplete
+//= require jquery-ui-1.9.2/jquery.ui.position
+//= require jquery-ui-1.9.2/jquery.ui.effect
+//= require jquery-ui-1.9.2/jquery.ui.effect-highlight
+//= require jquery.ui.datepicker
+//= require jquery.validate
+//= require jquery.validate.additional-methods
 //
 // Required by Blacklight
 //= require blacklight/blacklight
 //= require_tree .
-//= require sufia
+//
+//= require bootstrap-dropdown
+//= require bootstrap-button
+//= require bootstrap-modal
+//= require bootstrap-collapse
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-tab
 //
 // ORA custom
 //= require autocomplete
 //= require formUI
 //= require multiForm
+//= require orauploader
 //= require fileupload
 //= require ora
 //= require ora_form_progress
-//= require jquery.ui.datepicker
+//
+// require batch_edit
+// require terms_of_service
+// require sufia/permissions
+// require sufia/multiForm
+// require sufia/edit_metadata
+// require hydra/batch_select
+// this needs to be after batch_select so that the form ids get setup correctly
+// require sufia/batch_edit
