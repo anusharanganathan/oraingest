fd1c57d434030dc766ad120b526ca78769a19e5c
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-04-27 14:39:47 +0100

    Check for file types and limits moved to uploadAdd and form submit button disabled / enabled as needed

--- app/assets/javascripts/orauploader.js
+++ app/assets/javascripts/orauploader.js
@@ -8,10 +8,10 @@
 /*global $ */
 
 
-//200 MB  max file size 
+//2 GB  max file size 
 var max_file_size = 2000000000;
 var max_file_size_str = "2 GB";
-//500 MB max total upload size
+//5 GB max total upload size
 var max_total_file_size = 5000000000;
 var max_file_count = 100;
 var max_total_file_size_str = "5 GB";
@@ -28,34 +28,36 @@ var sequentialUploads = true;
     // option afterSubmit: function(form, event, data)
     var settings = $.extend( { }, options);
     var files_done =0;      
-    var error_string ='';      
+    var error_string =''; 
+
+    function saveForm() {
+      $(".ora-validate-form").validate().settings.ignore = "*";
+      $("#workflow_submit_entries_status").val($(this).attr("data-default-value"));
+      $("#workflow_submit_entries_status").attr("value", $(this).attr("data-default-value"));
+      var focusid = $(document.activeElement).closest('[id]').attr('id');
+      if (focusid && $("form#new_record_fields #"+focusid).length) {
+        var inputid = "";
+        if ($("#"+focusid).attr("tag") != "input") {
+          inputid = $("#"+focusid).parent().closest('input').attr('id');
+        }
+        if (inputid) {
+          $("#redirect_field").val(inputid);
+          $("#redirect_field").attr("value", inputid);
+        } else {
+          $("#redirect_field").val(focusid);
+          $("#redirect_field").attr("value", focusid);
+        }
+      }
+      $("form#new_record_fields").submit();
+      //var loc = $("#redirect-loc").html();
+      //$(location).attr('href',loc);
+    }
 
     function uploadStopped() {
       if (files_done == filestoupload && (files_done >0)){
-        $(".ora-validate-form").validate().settings.ignore = "*";
-        $("#workflow_submit_entries_status").val($(this).attr("data-default-value"));
-        $("#workflow_submit_entries_status").attr("value", $(this).attr("data-default-value"));
-        var focusid = $(document.activeElement).closest('[id]').attr('id');
-        if (focusid && $("form#new_record_fields #"+focusid).length) {
-          var inputid = "";
-          if ($("#"+focusid).attr("tag") != "input") {
-            inputid = $("#"+focusid).parent().closest('input').attr('id');
-          }
-          if (inputid) {
-            $("#redirect_field").val(inputid);
-            $("#redirect_field").attr("value", inputid);
-          } else {
-            $("#redirect_field").val(focusid);
-            $("#redirect_field").attr("value", focusid);
-          }
-        }
-        $("form#new_record_fields").submit();
-        //var loc = $("#redirect-loc").html();
-        //$(location).attr('href',loc);
+        saveForm();
       } else if (error_string.length > 0){
-        //TODO: Enable the save button
-        //$("#new_record_fields").find('input[type="submit"]').removeAttr('disabled');
-        // an error occured       
+        // an error occured
         if (files_done == 0) {
           $("#fail").fadeIn('slow')
         } else {
@@ -67,40 +69,38 @@ var sequentialUploads = true;
     } 
 
     function uploadAdd(e, data) {
-      filestoupload++;
-      if ( $('#terms_of_service').is(':checked') )
-        $('#main_upload_start').attr('disabled', false);
-    }
-
-    function uploadAdded(e, data) {
+      var total_sz = parseInt($('#total_upload_size').val()) + data.files[0].size;
+      // is file of wrong type
       if (data.files[0].error == 'acceptFileTypes'){
+        $("#errmsg").html(data.files[0].name + " is not of accepted type.");
         $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
       }
-      var total_sz = parseInt($('#total_upload_size').val()) + data.files[0].size;
       // is file size too big
-      if (data.files[0].size > max_file_size) {
-        $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
+      else if (data.files[0].size > max_file_size) {
         $("#errmsg").html(data.files[0].name + " is too big. No files over " + max_file_size_str + " can be uploaded.");
         $("#errmsg").fadeIn('slow');
+        $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
       }
       // cumulative upload file size is too big
       else if( total_sz > max_total_file_size) {
         if (first_file_after_max == '') first_file_after_max = data.files[0].name;
-        $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
         $("#errmsg").html("All files selected from " + first_file_after_max + " and after will not be uploaded because your total upload is too big. You may not upload more than " + max_total_file_size_str + " in one upload.");
         $("#errmsg").fadeIn('slow');
+        $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
       }
       else if( filestoupload > max_file_count) {
         if (first_file_after_max == '') first_file_after_max = data.files[0].name;
-        $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
         $("#errmsg").html("All files selected from " + first_file_after_max + " and after will not be uploaded because your total number of files is too big. You may not upload more than " + max_file_count + " files in one upload.");
         $("#errmsg").fadeIn('slow');
-      }
+        $($('#fileupload .files .cancel button')[data.context[0].rowIndex]).click(); 
+      } 
       else {
+        filestoupload++;
         $('#total_upload_size').val( parseInt($('#total_upload_size').val()) + data.files[0].size );
+        if ( $('#terms_of_service').is(':checked') )
+          $('#main_upload_start').attr('disabled', false);
       }
     }
-
 
     function uploadDone(e, data) {
       var file = ($.isArray(data.result) && data.result[0]) || {error: 'emptyResult'};
@@ -124,28 +124,35 @@ var sequentialUploads = true;
     // and assign them to a value in the form that is being uploaded:
     // based off of https://github.com/blueimp/jQuery-File-Upload/wiki/How-to-submit-additional-Form-Data
     function uploadSubmit(e, data) {
-      // TODO: disable the save button in form once upload has started
-      //$("#new_record_fields").find('input[type="submit"]').attr('disabled','disabled');
-      //$('#relative_path').val(data.files[0].webkitRelativePath);
       if (settings.afterSubmit) {
         settings.afterSubmit(this, e, data);
       }
+      $("form#new_record_fields :submit").attr("title", "The form will be saved on completion of file upload");
+      $("form#new_record_fields :submit").attr("disabled", true);
     }
 
     function uploadFail(e, data) {
       if (data.errorThrown == 'abort') {
          filestoupload--;
-         if ((files_done == filestoupload)&&(files_done >0)){
-             var loc = $("#redirect-loc").html()+"?file_count="+filestoupload
-             $(location).attr('href',loc);
-         }
          $('#total_upload_size').val( parseInt($('#total_upload_size').val()) - data.files[0].size );
-         
+         if ((files_done == filestoupload)&&(files_done >0)){
+           saveForm();
+         } else {
+           $("#errmsg").html(error_string);
+         }
+         if (filestoupload == 0 && files_done == 0) {
+           $("form#new_record_fields :submit").attr("title", "");
+           $("form#new_record_fields :submit").removeAttr("disabled");
+         }
       } else {
        if (error_string.length > 0) {
           error_string += '<br/>';
        }
        error_string += data.errorThrown + ": " + data.textStatus;
+       $("#errmsg").html(error_string);
+       $("#errmsg").fadeIn('slow');
+       $("form#new_record_fields :submit").attr("title", "");
+       $("form#new_record_fields :submit").removeAttr("disabled");
       }
     }
 
@@ -170,15 +177,18 @@ var sequentialUploads = true;
           /^[^\.].*$/i
       );
 
+      $('#fileupload').fileupload(
+          'option',
+          'sequentialUploads',
+          true
+      );
+
       $('#fileupload').bind("fileuploadstop", uploadStopped);
 
       // count the number of uploaded files to send to edit
+      // check the validation on if the file type is not accepted just click cancel for the user as we do not want them to see all the hidden files
       $('#fileupload').bind("fileuploadadd", uploadAdd);
       
-      
-      // check the validation on if the file type is not accepted just click cancel for the user as we do not want them to see all the hidden files
-      $('#fileupload').bind("fileuploadadded", uploadAdded);
-
       // count the number of files completed and ready to send to edit                          
       $('#fileupload').bind("fileuploaddone", uploadDone);
 
