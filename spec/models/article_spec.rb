ba7473d1b0d9d60e456a8679b8ca26afbffa7569
Paul Coleman <pcoleman@coleman.co.uk>
2015-05-11 11:14:06 +0100

    added  tests for Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -52,4 +52,80 @@ describe Article do
     end
 
   end
+
+  describe 'when creating a new article' do
+    before do
+      @article = Article.new
+    end
+
+    it 'initializes the submission workflow' do
+      @article.save
+      expect(@article.workflows).not_to be_empty
+    end
+
+    it 'removes blank assertions' do
+      @article.title = 'Test title'
+      @article.subtitle = ''
+      @article.save
+      expect(@article.title).to eq(['Test title'])
+      expect(@article.subtitle).to eq([])
+      expect(@article.keyword).to eq([])
+    end
+  end
+
+  describe 'applying permissions' do
+    before do
+      @article = Article.new
+      @reviewer = FactoryGirl.find_or_create(:reviewer)
+      @article.apply_permissions(@reviewer)
+    end
+
+    it 'should set the permisions' do
+      expect(@article.permissions).not_to be_empty
+    end
+
+    it 'sets the permissions to reviewer/group/edit' do
+      permission = @article.permissions.first
+      expect(permission.name).to eq('reviewer')
+      expect(permission.type).to eq('group')
+      expect(permission.access).to eq('edit')
+    end
+  end
+
+  describe '#to_jq_upload' do
+    before do
+      @article = Article.new
+      @jq_upload = @article.to_jq_upload('title', 120, 'uuid:nn999n999', 'dsid')
+    end
+
+    it 'creates the jq upload params' do
+      expect(@jq_upload).to be_a(Hash)
+    end
+
+    it 'sets the name' do
+      expect(@jq_upload['name']).to eq('title')
+    end
+
+    it 'sets the size' do
+      expect(@jq_upload['size']).to eq(120)
+    end
+
+    it 'sets the url' do
+      expect(@jq_upload['url']).to eq('/articles/uuid:nn999n999/file/dsid')
+    end
+
+    it 'sets the thumbnail url' do
+      expect(@jq_upload['thumbnail_url']).to eq('fileIcons/default-icon-48x48.png')
+    end
+
+    it 'sets the delete url' do
+      expect(@jq_upload['delete_url']).to eq('/articles/uuid:nn999n999/file/dsid')
+    end
+
+    it 'sets the delete type' do
+      expect(@jq_upload['delete_type']).to eq('DELETE')
+    end
+
+  end
+
 end
