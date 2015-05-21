b11a584fbac12d5186519874eaeab37cd657f8eb
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2015-05-21 15:10:00 +0100

    Check for model dataset, changed doi_requested method to question and modified if statements

--- app/models/concerns/doi_methods.rb
+++ app/models/concerns/doi_methods.rb
@@ -5,10 +5,13 @@ module DoiMethods
 
   def doi(mint=true)
     doi = nil
-    if (!self.publication.nil? && !self.publication[0].nil? &&
-       !self.publication[0].hasDocument.nil? && !self.publication[0].hasDocument[0].nil? &&
-       !self.publication[0].hasDocument[0].doi.nil? && !self.publication[0].hasDocument[0].doi[0].blank?)
-      doi = self.publication[0].hasDocument[0].doi[0]
+    if self.class.model_name.to_s != "Dataset"
+      return doi
+    end
+    if (self.publication.first && 
+        self.publication.first.hasDocument.first &&
+        !self.publication.first.hasDocument.first.doi.first.blank?)
+      doi = self.publication.first.hasDocument.first.doi.first
     elsif mint
       doi = Sufia::Noid.noidify(Sufia::IdService.mint_doi)
       doi = Sufia::Noid.doize(doi)
@@ -16,9 +19,9 @@ module DoiMethods
     doi
   end
 
-  def doi_requested
+  def doi_requested?
     status = false
-    if !self.workflows.nil? && !self.workflows.first.nil? && !self.workflows.first.involves.nil?
+    if self.workflows.first && self.workflows.first.involves.any?
       self.workflows.first.involves.each do |event|
         if event.include?("Register doi")
           status = true
@@ -29,6 +32,9 @@ module DoiMethods
   end
 
   def doi_data
+    if self.class.model_name.to_s != "Dataset"
+      return {}
+    end
     contributorTypes = [
       "ContactPerson",
       "DataCollector",
@@ -65,96 +71,98 @@ module DoiMethods
       description: []
     }
     # title
-    if !self.title.empty?
-      doi_data[:title] = self.title[0]
+    unless self.title.first.blank?
+      doi_data[:title] = self.title.first
     end
     # identifier
     begin
-      doi_data[:identifier] = self.publication[0].hasDocument[0].doi[0]
+      doi_data[:identifier] = self.publication.first.hasDocument.first.doi.first
     rescue
       doi_data[:identifier] = ""
     end
     # publisher
     begin
-      doi_data[:publisher] = self.publication[0].publisher[0].agent[0].name[0]
+      doi_data[:publisher] = self.publication.first.publisher.first.agent.first.name.first
     rescue
       doi_data[:publisher] = "University of Oxford"
     end
     # publication year
     begin
-      doi_data[:publicationYear] = self.publication[0].datePublished[0]
+      doi_data[:publicationYear] = self.publication.first.datePublished.first
     rescue
       doi_data[:publicationYear] = Time.now.year
     end
     # creator and contributor
-    self.creation[0].creator.each do |cr|
-      if !cr.agent.nil? && !cr.agent[0].nil? && !cr.agent[0].name.nil? && !cr.agent[0].name[0].empty?
-        c = { name: cr.agent[0].name[0] }
-        if !cr.agent[0].affiliation.nil? && !cr.agent[0].affiliation[0].nil? && !cr.agent[0].affiliation[0].name.nil? && !cr.agent[0].affiliation[0].name[0].empty?
-          c[:affiliation] = cr.agent[0].affiliation[0].name[0]
-        end
-        if cr.role[0].to_s == "http://purl.org/dc/terms/creator"
-          doi_data[:creator] << c
-        else
-          if Sufia.config.role_labels.include?(cr.role[0]) && contributorTypes.include?(Sufia.config.role_labels[cr.role[0]])
-            c[:type] = Sufia.config.role_labels[cr.role[0]]
-          elsif contributorTypes.include?(cr.role[0])
-            c[:type] = cr.role[0]
-          else
-            c[:type] = "Other"
+    if self.creation.first
+      self.creation.first.creator.each do |cr|
+        if cr.agent.first && !cr.agent.first.name.first.blank?
+          c = { name: cr.agent.first.name.first }
+          if (cr.agent.first.affiliation.first &&
+             !cr.agent.first.affiliation.first.name.first.blank?)
+            c[:affiliation] = cr.agent.first.affiliation.first.name.first
           end
-          doi_data[:contributor] << c
+          if cr.role.first.to_s == "http://purl.org/dc/terms/creator"
+            doi_data[:creator] << c
+          else
+            if (Sufia.config.role_labels.include?(cr.role.first) &&
+               contributorTypes.include?(Sufia.config.role_labels[cr.role.first]))
+              c[:type] = Sufia.config.role_labels[cr.role.first]
+            elsif contributorTypes.include?(cr.role.first)
+              c[:type] = cr.role.first
+            else
+              c[:type] = "Other"
+            end
+            doi_data[:contributor] << c
+          end
         end
       end
     end
     # subject
     self.subject.each do |s|
       sh = {}
-      if !s.subjectLabel.empty? && !s.subjectLabel[0].empty?
-        sh[:subject] = s.subjectLabel[0]
+      unless s.subjectLabel.first.blank?
+        sh[:subject] = s.subjectLabel.first
       end
-      if !s.subjectAuthority.empty? && !s.subjectAuthority[0].empty?
-        sh[:schemeUri] = s.subjectAuthority[0].rpartition("/")[0]
+      unless s.subjectAuthority.first.blank?
+        sh[:schemeUri] = s.subjectAuthority.first.rpartition("/").first
       end
-      if !s.subjectScheme.empty? && !s.subjectScheme[0].empty?
-        sh[:scheme] = s.subjectScheme[0]
+      unless s.subjectScheme.first.blank?
+        sh[:scheme] = s.subjectScheme.first
       end
-      if !sh.empty?
+      if sh.any?
         doi_data[:subject] << sh
       end
     end
     # digitalSize
-    if !self.digitalSize.empty?
-      doi_data[:digitalSize] = self.digitalSize[0]
-    end
-    # format
-    if !self.format.empty?
-      doi_data[:format] = self.format[0]
+    unless self.digitalSize.first.blank?
+      doi_data[:digitalSize] = self.digitalSize.first
     end
     # version
-    if !self.version.empty?
-      doi_data[:version] = self.version[0]
+    unless self.version.first.blank?
+      doi_data[:version] = self.version.first
     end
     # rights
-    if !self.license.nil?
+    if self.license.any?
       self.license.each do |l|
         r =  {}
-        if !l.licenseStatement.empty? && !l.licenseStatement[0].empty?
-          r[:rights] = l.licenseStatement[0]
-        elsif !l.licenseLabel.empty? && !l.licenseLabel[0].empty?
-          r[:rights] = l.licenseLabel[0]
+        unless l.licenseStatement.first.blank?
+          r[:rights] = l.licenseStatement.first
+        else
+          unless l.licenseLabel.first.blank?
+            r[:rights] = l.licenseLabel.first
+          end
         end
-        if !l.licenseURI.empty? && !l.licenseURI[0].empty?
-          r[:rightsUri] = l.licenseURI[0]
+        unless l.licenseURI.first.blank?
+          r[:rightsUri] = l.licenseURI.first
         end
-        if !r.empty?
+        if r.any?
           doi_data[:rights] << r
         end
       end
     end
     # abstract
-    if !self.abstract.empty?
-      doi_data[:description] << {description: self.abstract[0], type: "Abstract"}
+    unless self.abstract.first.blank?
+      doi_data[:description] << {description: self.abstract.first, type: "Abstract"}
     end
     doi_data
   end
@@ -173,5 +181,4 @@ module DoiMethods
   end
 
 end
-
 
