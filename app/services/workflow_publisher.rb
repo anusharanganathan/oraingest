1e8dea12a07b7c062886b44e954ec126968b3184
Paul Coleman <pcoleman@coleman.co.uk>
2015-05-29 08:32:55 +0100

    Refactor use WorkflowPublisher class instead of WorkflowMethods concerns

--- app/services/workflow_publisher.rb
+++ app/services/workflow_publisher.rb
@@ -5,113 +5,119 @@ class WorkflowPublisher
   def initialize(model)
     @parent_model
   end
-
+  
   def perform_action(current_user)
-    model_klass = parent_model.model_klass
     # send email
     models = { "Article" => 'articles', "DatasetAgreement" => "dataset_agreements", "Dataset" => "datasets" }
-    record_url = Rails.application.routes.url_helpers.url_for(:controller => models[model_klass], :action=>'show', :id => parent_model.id)
-    data = {"record_id" => parent_model.id, "record_url" => record_url, "doi_requested"=>parent_model.doi_requested}
-    if parent_model.doi_requested
+    record_url = Rails.application.routes.url_helpers.url_for(:controller => models[parent_model.model_klass], :action=>'show', :id => parent_model.id)
+    data = {"record_id" => parent_model.id, "record_url" => record_url, "doi_requested"=>parent_model.doi_requested?}
+    if parent_model.doi_requested?
       data["doi"] = parent_model.doi(mint=false)
     end
-    ans = parent_model.datastreams["workflowMetadata"].send_email("MediatedSubmission", data, current_user, model_klass)
+    ans = parent_model.datastreams["workflowMetadata"].send_email("MediatedSubmission", data, current_user, parent_model.model_klass)
     # publish record
-    publishRecord("MediatedSubmission", current_user)
+    publish_record("MediatedSubmission", current_user)
   end
 
-  def publishRecord(wf_id, current_user)
+  def publish_record(wf_id, current_user)
     # Send pid and list of open datastreams to queue
     # If datastreams are empty, that means record is all dark
-    wf = parent_model.workflows.select{|wf| wf.identifier.first == wf_id}.first
-    model_klass = parent_model.model_klass
-    #Use Sufia.config.publish_to_queue_options to determine if method needs to be called
-    if wf && Sufia.config.publish_to_queue_options.keys.include?(model_klass.downcase) && Sufia.config.publish_to_queue_options[model_klass.downcase].include?(wf.current_status)
-      # The status is available for this model in the config
-      occurences = wf.all_statuses.select{|s| s == wf.current_status}
-      occurence = Sufia.config.publish_to_queue_options[model_klass.downcase][wf.current_status]['occurence']
-      if (occurences.length == occurence) || occurence == "all"
-        # The occurence count matches and so procedd to performing action
-        toMigrate = false
-        msg = []
-        datastreams = []
-        numberOfFiles = 0
-        #Get list of all datastreams without access rights
-        conts = parent_model.datastreams.keys.select { |key| key.start_with?('content') and parent_model.datastreams[key].content != nil }
-        parent_model.hasPart.each do |hp|
-          if conts.include?(hp.identifier[0])
-            numberOfFiles = numberOfFiles + 1
-            if !hp.accessRights.nil? && !hp.accessRights[0].embargoStatus.nil? && !hp.accessRights[0].embargoStatus[0].nil? && !hp.accessRights[0].embargoStatus[0].empty?
-              conts.delete(hp.identifier[0])
-            end
-          end
-        end
-        # If access rights not defined for file or catalogue record, mark as system failure
-        if !conts.empty? || parent_model.accessRights.nil? || parent_model.accessRights[0].embargoStatus.nil?
-          status = "System failure"
-          if parent_model.accessRights.nil? || parent_model.accessRights[0].embargoStatus.nil?
-            msg << "No embargo details for catalogue record."
-          end
-          if !conts.empty?
-            conts.each do |ds|
-              msg << "No embargo details for #{ds}."
-            end
-          end
-          # If catalogue record is open access, gather datastreams to migrate
-        elsif parent_model.accessRights[0].embargoStatus[0] == "Open access"
-          if parent_model.datastreams.keys().include? "descMetadata"
-            status = "System verified"
-            toMigrate = true
-            datastreams << "descMetadata"
-            if parent_model.datastreams.keys().include? "relationsMetadata"
-              datastreams << "relationsMetadata"
-            end
-            parent_model.hasPart.each do |hp|
-              if hp.accessRights[0].embargoStatus[0] == "Open access"
-                datastreams << "#{hp.identifier[0]}"
-              end
-            end
-            msg << "Datastreams to migrate: %s."%datastreams.join(", ")
-          else
-            msg << "No descMetadata available."
-            status = "System failure"
-          end
-          # If catalogue record is not open access, no datastreams to gather
-        elsif parent_model.accessRights[0].embargoStatus[0] != "Open access"
-          toMigrate = true
-          status = "System verified" #Set to Migrate, depending on archive policy
-          msg << "Catalogue record is #{parent_model.accessRights[0].embargoStatus[0]}."
-        end
-        # Update status of object
-        parent_model.workflows.first.entries.build(description:msg.join(" "), creator:current_user, date:Time.now.to_s, status:status)
-        # Push object to queue
-        if toMigrate
-          if model_klass == "Dataset"
-            Resque.enqueue(DatabankPublishRecordJob, parent_model.id.to_s, datastreams, model_klass, numberOfFiles.to_s)
-          else
-            # Add to ora publish queue
-            args = {
-                'pid' => parent_model.id.to_s,
-                'datastreams' => datastreams,
-                'model' => model_klass,
-                'numberOfFiles' => numberOfFiles.t_s
-            }
-            Resque.redis.rpush(Sufia.config.ora_publish_queue_name, args.to_json)
-          end
-        end
-        #else
-        #  # Note: Not doing this as we may just add a whole lot of comments for redundant clicks
-        #  # Cannot publish as not not the correct occurence. Add comment and return
-        #  msg = "Record cannot be processed for count of current status is #{occurence.to_s}."
-        #  comment = {:description => msg, :creator => current_user, :date => Time.now.to_s}
-        #  parent_model.workflows.first.comments.build(comment)
-      end
-      #else
-      #  # Note: Not doing this as we may just add a whole lot of comments for redundant clicks
-      #  # Cannot publish as not approved. Add comment and return
-      #  msg = "Record cannot be processed for current status #{parent_model.workflows.first.current_status}."
-      #  comment = {:description => msg, :creator => current_user, :date => Time.now.to_s}
-      #  parent_model.workflows.first.comments.build(comment)
+    unless ready_to_publish?(wf_id=wf_id)
+      return
     end
+    ans, msg = check_minimum_metadata
+    unless ans
+      status = "System failure"
+    else
+      status = "System verified"
+      open_access_content = parent_model.list_open_access_content
+      numberOfFiles = (open_access_content.select { |key| key.start_with?('content') }).length
+      msg << "Open access datastreams: %s."%open_access_content.join(", ")
+      if parent_model.model_klass == "Dataset"
+        Resque.enqueue(DatabankPublishRecordJob, parent_model.id.to_s, open_access_content, parent_model.model_klass, numberOfFiles.to_s)
+      else
+        # Add to ora publish queue
+        args = {
+            'pid' => parent_model.id.to_s,
+            'datastreams' => open_access_content,
+            'model' => parent_model.model_klass,
+            'numberOfFiles' => numberOfFiles.to_s
+        }
+        Resque.redis.rpush(Sufia.config.ora_publish_queue_name, args.to_json)
+      end
+    end
+    update_status(status, msg)
   end
+
+  def ready_to_publish?(wf_id="MediatedSubmission")
+    wf = parent_model.workflows.select{|wf| wf.identifier.first == wf_id}.first
+    status = false
+    if wf.nil?
+      return status
+    end
+    unless Sufia.config.publish_to_queue_options.keys.include?(parent_model.model_klass.downcase)
+      return status
+    end
+    unless Sufia.config.publish_to_queue_options[parent_model.model_klass.downcase].include?(wf.current_status)
+      return status
+    end
+    occurences = wf.all_statuses.select{|s| s == wf.current_status}
+    occurence = Sufia.config.publish_to_queue_options[parent_model.model_klass.downcase][wf.current_status]['occurence']
+    return (occurences.length == occurence) || occurence == "all"
+  end
+
+  def check_minimum_metadata
+    status = true
+    msg = []
+    # descMetadata has to exist
+    unless parent_model.datastreams.keys().include? 'descMetadata'
+      status = false
+      msg << 'No descMetadata available.'
+    end
+    # All of the access rights should be in place
+    unless parent_model.has_all_access_rights?
+      status = false
+      msg << 'Not all files or the catalogue record has embargo details'
+    end
+    # The metadata for regsitering DOI should exist
+    if parent_model.model_klass == 'Dataset' && parent_model.doi_requested?
+      unless parent_model.doi_registered?
+        payload = parent_model.doi_data
+        dd = ORA::DataDoi.new(Sufia.config.doi_credentials)
+        # validate required fields
+        begin
+          dd.validate_required(payload)
+        rescue ORA::DataValidationError => e
+          status = false
+          msg << e.message
+        end
+        # validate xml to schema
+        begin
+          dd.validate_xml(payload)
+        rescue ORA::DataValidationError => e
+          status = false
+          msg << e.message
+        end
+      end
+    end
+    return status, msg
+  end
+
+  def update_status(status, description, creator='ORA Deposit system', wf_id="MediatedSubmission")
+    #Update the workflow status. Add a new workflow entry.
+    unless Sufia.config.workflow_status.include?(status)
+      return false
+    end
+    wf = parent_model.workflows.select{|wf| wf.identifier.first == wf_id}.first
+    wf.entries.build
+    wf.entries.last.status = Sufia.config.workflow_status[status]
+    wf.entries.last.creator = creator
+    if description.is_a?(Array)
+      description = description.join('\n')
+    end
+    wf.entries.last.description = description
+    wf.entries.last.date = Time.now.to_s
+    return true
+  end
+
 end
